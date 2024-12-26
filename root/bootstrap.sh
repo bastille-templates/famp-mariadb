d476939c90b5f937ecfc3e4109375381dcbc9e50
Asep Ready <savalunova@gmail.com>
2024-12-26 12:06:03 +0700

    fix

--- root/bootstrap.sh
+++ root/bootstrap.sh
@@ -4,34 +4,28 @@ sed -i -e "s|\$cfg\['blowfish_secret'\] = .*;|\$cfg['blowfish_secret'] = \\\sodi
 echo 'Verify .....'
 grep "blowfish_secret" /usr/local/www/phpMyAdmin/config.inc.php
 
-DB_ROOT_PASSWORD=$(openssl rand -base64 16) && export DB_ROOT_PASSWORD && echo $DB_ROOT_PASSWORD >> /root/db_root_pwd.txt
+#pkg install -y expect
+#DB_ROOT_PASSWORD=$(openssl rand -base64 32) && export DB_ROOT_PASSWORD && echo $DB_ROOT_PASSWORD > /root/db_root_pwd.txt
 
-SECURE_MYSQL=$(expect -c "
-set timeout 10
-set DB_ROOT_PASSWORD "$DB_ROOT_PASSWORD"
-spawn mysql_secure_installation
-expect \"Press y|Y for Yes, any other key for No:\"
-send \"y\r\"
-expect \"Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:\"
-send \"0\r\"
-expect \"New password:\"
-send \"$DB_ROOT_PASSWORD\r\"
-expect \"Re-enter new password:\"
-send \"$DB_ROOT_PASSWORD\r\"
-expect \"Do you wish to continue with the password provided?(Press y|Y for Yes, any other key for No) :\"
-send \"Y\r\"
-expect \"Remove anonymous users?\"
-send \"y\r\"
-expect \"Disallow root login remotely?\"
-send \"y\r\"
-expect \"Remove test database and access to it?\"
-send \"y\r\"
-expect \"Reload privilege tables now?\"
-send \"y\r\"
-expect eof
-")
+#SECURE_MYSQL=$(expect -c "
+#set timeout 10
+#spawn mysql_secure_installation
+#expect \"Switch to unix_socket authentication\"
+#send \"n\r\"
+#expect \"Change the root password?\"
+#send \"n\r\"
+#expect \"Remove anonymous users?\"
+#send \"y\r\"
+#expect \"Disallow root login remotely?\"
+#send \"y\r\"
+#expect \"Remove test database and access to it?\"
+#send \"y\r\"
+#expect \"Reload privilege tables now?\"
+#send \"y\r\"
+#expect eof
+#")
 
-echo "$SECURE_MYSQL"
+#echo "$SECURE_MYSQL"
 
 # Display the location of the generated root password for MySQL
 echo "Your DB_ROOT_PASSWORD is written on this file /root/db_root_pwd.txt"
